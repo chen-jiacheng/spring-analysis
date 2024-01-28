60a85aaf296dd389b100204c335dd2f711765ace
chenjiacheng <chen-jiacheng@foxmail.com>
2024-01-29 00:42:50 +0800

    feat(mybatis): init

---
+++ spring-analysis-mybatis/src/main/java/com/chenjiacheng/mybatis/SpringAnalysisMyBatisApplication.java
@@ -0,0 +1,13 @@
+package com.chenjiacheng.mybatis;
+
+/**
+ * Created by chenjiacheng on 2024/1/29 00:38
+ *
+ * @author chenjiacheng
+ * @since 1.0.0
+ */
+public class SpringAnalysisMyBatisApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
